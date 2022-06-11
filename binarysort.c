0d8e6238137e02affdf93594ad870088623c36a7
Rutvi Thakkar <thakkarrutvi2002@gmail.com>
2022-06-11 12:12:21 +0530

    Practice

---
+++ binarysort.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+#define SIZE 7
+void main()
+{
+	int a[SIZE],key,low=0,high=SIZE-1;
+	int i,j,flag=-1;
+	clrscr();
+	for(i=0;i<SIZE;i++)
+	{
+		printf("Enter The value of a[%d]",i);
+		scanf("%d",&a[i]);
+	}
+	printf("\n The array is \n");
+	for(i=0;i<SIZE;i++)
+	{
+		printf("\t%d",a[i]);
+	}
+	printf("\n Enter the key: ");
+	scanf("%d",&key);
+	while(low<=high)
+	{
+		mid=(low+high)/2;
+		if(key==a[mid])
+		{
+			flag=mid;
+			break;
+		}
+		else if(key>a[mid])
+		{
+			low=mid+1;	
+		}
+		else
+		{
+			high=mid-1;
+		}
+	}
+	if(flag==-1)
+	{
+		printf("\n Key not found!");
+	}
+	else
+	{
+		printf("\n Key found");
+	}
+	getch();
+}
